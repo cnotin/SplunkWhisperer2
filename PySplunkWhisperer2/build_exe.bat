b4f978ef308ae4bc289d00f37a386bd986b19e49
Clément Notin <cnotin@tenable.com>
2022-09-30 18:39:52 +0200

    Keep python2 as the default files

---
+++ PySplunkWhisperer2/build_exe.bat
@@ -0,0 +1,5 @@
+pyinstaller.exe PySplunkWhisperer2_local.py --onefile --name PySplunkWhisperer2
+@echo.
+@echo.
+@echo Finished! EXE available in "dist" folder
+@pause
--- PySplunkWhisperer2/build_exe_python2.bat
+++
@@ -1,5 +0,0 @@
-pyinstaller.exe PySplunkWhisperer2_local_python2.py --onefile --name PySplunkWhisperer2
-@echo.
-@echo.
-@echo Finished! EXE available in "dist" folder
-@pause
